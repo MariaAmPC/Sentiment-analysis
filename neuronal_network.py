cdb84ad999268860b95ff3d2bedb37c331943050
MariaAmPC <maryloe02@gmail.com>
2024-05-08 13:19:10 +0200

    Update neuronal_network.py

--- neuronal_network.py
+++ neuronal_network.py
@@ -3,13 +3,12 @@ import pandas as pd
 import pathlib
 import requests
 from io import BytesIO
+import matplotlib.pyplot as plt
 
 #Größe des Netzwerkes festlegen
-gr_in=784
-gr_l1=30
-gr_l2=30
-gr_l3=30
-gr_out=10
+gr_in=5
+gr_l1=10
+gr_out=2
 
 
 #Einlesen von anderem Datensatz, nur zum testen des NN. Wird danach wieder gelöscht
@@ -48,23 +47,23 @@ def forward(bias, layer, x):
     return(sigmoid(pre))
     
 
-"""
-
-# Alternatives Sätze einlesen für alle Zeichen im satz 
-# Ergebnis ist ein Array von einem Arrys welche den UTF-8 code pro Zeichen haben
+""""
 sentences=[]
 for i in df_train.head(5)['sentence'].values:
-    sec=[]
+    sec=np.array([])
     for n in i: 
-        sec.append(ord(n))
+        sec=np.append(sec,ord(n))
+    #print(sec)
     sentences.append(sec)
+   # print(sentences)
 
-"""
+""""
 
 #Sätze einlesen
 sentences=np.empty((0,5))
 for i in df_train.head(50)['sentence'].values:
     sentences = np.insert(sentences, len(sentences), np.array([[ord(i[0]),ord(i[1]),ord(i[2]),ord(i[3]),ord(i[4])]]),axis=0)
+print(sentences)
 
   
 #Labels einlesen
@@ -80,34 +79,28 @@ for i in df_train.head(50)['label'].values:
 
 #Gewichte der Neuronenverbindugnen zufällig festlegen
 w_i_l1 = np.random.uniform(-0.5,0.5,(gr_l1,gr_in))
-w_l1_l2 = np.random.uniform(-0.5,0.5,(gr_l2,gr_l1))
-w_l2_l3 = np.random.uniform(-0.5,0.5,(gr_l3,gr_l2))
-w_l3_o = np.random.uniform(-0.5,0.5,(gr_out,gr_l3))
+w_l1_o = np.random.uniform(-0.5,0.5,(gr_out,gr_l1))
 
 
 #Gewichte der Biases auf 0 setzen
 b_i_l1 = np.zeros((gr_l1,1))
-b_l1_l2 = np.zeros((gr_l2,1))
-b_l2_l3 = np.zeros((gr_l3,1))
-b_l3_o = np.zeros((gr_out,1))
+b_l1_o = np.zeros((gr_out,1))
 
 count = 20 #Anzahl der Durchläufe
 correct = 0 #Anzahl korrekten Ergebnisse
 learnrate = .01
 
 for counter in range(count):
-    for sentence,label in zip(images,imlabels):
+    for sentence,label in zip(sentences,labels):
 
         sentence.shape+=(1,)
         label.shape+=(1,)
-        print(sentence)
+        #print(sentence)
         #print(label)
 
         #foreward propagation
         l1 = forward(b_i_l1, w_i_l1, sentence)
-        l2 = forward(b_l1_l2, w_l1_l2, l1)
-        l3 = forward(b_l2_l3, w_l2_l3, l2)
-        out = forward(b_l3_o, w_l3_o, l3)
+        out = forward(b_l1_o, w_l1_o, l1)
     
         #print(out)
 
@@ -118,24 +111,31 @@ for counter in range(count):
 
         #Derivative berechnen: Backpropagation-weights * Ableitung von Sigmoid
         delta_out = out - label
-        w_l3_o += -learnrate* delta_out @ np.transpose(l3)
-        b_l3_o += -learnrate* delta_out
+        w_l1_o += -learnrate* delta_out @ np.transpose(l1)
+        b_l1_o += -learnrate* delta_out
 
-        delta_l3 = np.transpose(w_l3_o) @ delta_out * (l3*(l3-1))
-        w_l2_l3 += -learnrate* delta_l3 @ np.transpose(l2)
-        b_l2_l3 += -learnrate* delta_l3
-
-        delta_l2 = np.transpose(w_l2_l3) @ delta_l3 * (l2*(l2-1))
-        w_l1_l2 += -learnrate* delta_l2 @ np.transpose(l1)
-        b_l1_l2 += -learnrate* delta_l2
-
-        delta_l1 = np.transpose(w_l1_l2) @ delta_l2 * (l1*(l1-1))
+        delta_l1 = np.transpose(w_l1_o) @ delta_out * (l1*(1-l1))
         w_i_l1 += -learnrate* delta_l1 @ np.transpose(sentence)
         b_i_l1 += -learnrate* delta_l1
+        
 
     #Ausgeben der Genauigkeit nach jeder Iteration    
     print(f"Acc: {round((correct/len(sentences))*100,2)} %")
     correct=0
- 
+""""
+while True:
+    index = int(input("Enter a number (0 - 59999): "))
+    img = images[index]
+    plt.imshow(img.reshape(28, 28), cmap="Greys")
 
-        
+    img.shape += (1,)
+    # Forward propagation input -> hidden
+    h_pre = b_i_l1 + w_i_l1 @ img.reshape(784, 1)
+    h = 1 / (1 + np.exp(-h_pre))
+    # Forward propagation hidden -> output
+    o_pre = b_l1_o + w_l1_o @ h
+    o = 1 / (1 + np.exp(-o_pre))
+
+    plt.title(f"Subscribe if its a {o.argmax()} :)")
+    plt.show()
+      """  
