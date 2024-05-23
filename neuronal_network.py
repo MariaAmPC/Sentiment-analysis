f8ced76bbc2131668496c9af80be7f7f6c12ad12
MariaAmPC <maryloe02@gmail.com>
2024-05-23 18:10:55 +0200

    Update neuronal_network.py

--- neuronal_network.py
+++ neuronal_network.py
@@ -10,13 +10,14 @@ from gensim.models import KeyedVectors
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from collections import Counter
+from sentence_transformers import SentenceTransformer
 
 
 #Größe des Netzwerkes festlegen (size = Anzahl der hiddenlayer + Input und Output)
 size=3
 gr=[0]*size
 
-gr[0]=300
+gr[0]=768
 gr[1]=30 
 gr[2]=6
 
@@ -38,34 +39,10 @@ def forward(bias, weight, x):
 
 def getgr():
     return gr[0]
-"""
--------EINLESEN DATEN-------
-"""
-"""
-#Einlesen von anderem Datensatz, nur zum testen des NN. Wird danach wieder gelöscht
-def get_mnist():
-    url = "https://raw.githubusercontent.com/MariaAmPC/hate-speach/main/mnist.npz"
-    response = requests.get(url)
-    content = BytesIO(response.content)
-    # Die Datei öffnen
-    with np.load(content) as f:
-        # Daten aus der Datei laden
-        images, labels = f["x_train"], f["y_train"]
-    images = images.astype("float32") / 255
-    images = np.reshape(images, (images.shape[0], images.shape[1] * images.shape[2]))
-    labels = np.eye(10)[labels]
-    return images, labels
 
-images,imlabels=get_mnist()
-images=images[:500]
-imlabels=imlabels[:500]
-"""
 #Daten einlesen
-url_test =("https://raw.githubusercontent.com/MariaAmPC/hate-speach/main/Testing_meme_dataset.csv")
-url_train=("https://raw.githubusercontent.com/MariaAmPC/hate-speach/main/Training_meme_dataset.csv")
-url_validation=("https://raw.githubusercontent.com/MariaAmPC/hate-speach/main/Validation_meme_dataset.csv")
-
 df=pd.read_csv(r"https://raw.githubusercontent.com/MariaAmPC/hate-speach/main/tweet_emotions.csv")
+
 df = df[df.sentiment != 'empty']
 df = df[df.sentiment != 'enthusiasm']
 df = df[df.sentiment != 'fun']
@@ -74,56 +51,10 @@ df = df[df.sentiment != 'anger']
 df = df[df.sentiment != 'relief']
 df = df[df.sentiment != 'surprise']
 
-
-stats = Counter(df["sentiment"])
-print(dict(stats))
-
-
 df_test, df_train = train_test_split(df , test_size=0.33, random_state=42)
 
-#df_test= pd.read_csv(url_test,index_col=0)
-#df_train= pd.read_csv(url_train,index_col=0)
-#df_validation= pd.read_csv(url_validation,index_col=0)
-    
-###Sätze einlesen die vektoren haben später eine größe von 300
-
-#Corpus definieren auf dem die Gewichte basieren
-corpus = df_test['content']
-
-#initialisierung des TF-IDF-Vektorisierers
-tfidf_vectorizer = TfidfVectorizer()
-tfidf_matrix = tfidf_vectorizer.fit_transform(corpus)
-tfidf_feature_names = tfidf_vectorizer.get_feature_names_out()
-
-# Laden des vortrainierten Word2Vec-Modells (sollte später nicht lokal passieren)
-
-word2vec_model = KeyedVectors.load_word2vec_format(r"C:\Users\49170\Documents\FAU\ML4B\GoogleNews-vectors-negative300.bin", binary=True)
-
-#funktion die uns den Vektor des Satztes returned 
-def get_sentence_vector(sentence):
-    tokens = word_tokenize(sentence.lower())
-    tfidf_vector = tfidf_vectorizer.transform([" ".join(tokens)]) #verwandeln des satzes in vertikales array
-    tfidf_scores = {word: tfidf_vector[0, tfidf_feature_names.tolist().index(word)] for word in tokens if word in tfidf_feature_names} #gewichtung der einzelnen wörter erfahren
-    
-    word_vectors = []
-    for word in tokens:
-        if word in word2vec_model.key_to_index and word in tfidf_scores:
-            word_vector = word2vec_model.get_vector(word) * tfidf_scores[word] #vektor aus dem word2vec_model mit dem gewichtetem vektor aus dem TF system
-            word_vectors.append(word_vector)
-    
-    #durchschnitt der gewichteten vektoren erlangen
-    if word_vectors:
-        sentence_vector = np.mean(word_vectors, axis=0)
-    else:
-        sentence_vector = np.zeros(word2vec_model.vector_size)
-    
-    return sentence_vector
-
-sentences=np.empty((0,300))
-for i in df_train['content'].values:
-    sentences = np.insert(sentences, len(sentences), get_sentence_vector(i),axis=0)
-#print(sentences)
-
+model = SentenceTransformer('bert-base-nli-mean-tokens')
+sentences = model.encode(df_train["content"].tolist())
   
 #Labels einlesen
 labels=np.empty((0,6))
@@ -153,7 +84,7 @@ for i in df_train['sentiment'].values:
 epoch = 20 #Anzahl der Epochen
 correct = 0 #Anzahl korrekte Ergebnisse
 count = 0 #Anzahl Durchläufe pro Epoche bzw. Testgröße
-learnrate = 0.005
+learnrate = 0.01
 
 for epoche in range(epoch):
     w_change=[0]*size
